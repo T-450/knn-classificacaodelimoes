dcc01c442ce5c715ab779d36fb39b1984f17ae0d
Edward Dias <edward.teixeira@growinco.com>
2020-10-25 19:47:19 -0300

    first commit

--- built/js/data/FileHandler.data.d.ts
+++
@@ -1,5 +0,0 @@
-export default class FileHandler {
-    private filename;
-    constructor(filename: string);
-    fromTextToJson(outfileName: string): Promise<any>;
-}
--- built/js/data/sample.service.d.ts
+++
@@ -1,7 +0,0 @@
-export default class SampleService {
-    private datasetPath;
-    private dataSetOutPath;
-    private _fileHandler;
-    constructor(datasetPath: string, dataSetOutPath: string);
-    getTrainingSample(): any;
-}
